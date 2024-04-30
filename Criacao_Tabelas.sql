06cd3862b1ae42db68f7883e90c489a765a084dd
Bruno-LS <111468518+Bruno-LS@users.noreply.github.com>
2024-04-30 07:39:40 -0300

    Add files via upload

---
+++ Criacao_Tabelas.sql
@@ -0,0 +1,63 @@
+
+CREATE TABLE Carga(
+	ID_Pedido int NOT NULL,
+	ID_Produto int NOT NULL,
+	Pedido_data varchar(10) NOT NULL,
+	UPC int NOT NULL,
+	Nome_Produto varchar(30) NOT NULL,
+	Quant int NOT NULL,
+	Valor float NOT NULL,
+	Frete float NOT NULL,
+	Email varchar(100) NOT NULL,
+	Nome_Comprador varchar(100) NOT NULL,
+	Endereco varchar(100) NOT NULL,
+	CEP int NOT NULL,
+	UF varchar(50) NOT NULL,
+	PAIS varchar(30) NOT NULL,
+	CPF VARCHAR(11) NOT NULL
+)
+GO
+
+CREATE TABLE Cliente(
+	ID_cliente int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	Nome varchar(100) NOT NULL,
+	CPF varchar(11) NOT NULL,
+	Email varchar(100) NOT NULL,
+
+)
+GO
+
+CREATE TABLE Pedido(
+	ID_Pedido int PRIMARY KEY NOT NULL,
+	Data_Pedido date NOT NULL,
+	Quant_Produto int NOT NULL,
+	Endereco_Pedido varchar(100) NOT NULL,
+	CEP int NOT NULL,
+	UF varchar(50) NOT NULL,
+	PAIS varchar(30) NOT NULL,
+	ID_Cliente int NOT NULL,
+	FOREIGN KEY (ID_Cliente) REFERENCES Cliente(ID_Cliente)
+)
+GO
+
+
+CREATE TABLE Itens_Pedidos
+(
+	ID_Produto int NOT NULL,
+	ID_Pedido int NOT NULL,
+	Quant int NOT NULL,
+	FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto),
+	FOREIGN KEY (ID_Pedido) REFERENCES Pedido(ID_Pedido),
+
+)
+GO
+
+
+CREATE TABLE Produto(
+	ID_Produto int PRIMARY KEY NOT NULL,
+	Valor float NOT NULL,
+	Nome_produto varchar(30) NOT NULL,
+	Frete_Produto float NULL,
+	UPC int NOT NULL,
+)
+GO
